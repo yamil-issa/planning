3e0fc459b3756d6705e47827af4a78351881226d
neo-askin <issayamilrt@gmail.com>
2023-02-16 16:28:07 +0100

    make model

--- view/index.php
+++ view/index.php
@@ -1,4 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>planning</title>
+</head>
+<body>
+    <h1>
+        Planning
+    </h1>
+</body>
+</html>
+
+
+
 <?php
-echo"<h1>Planning</h1>"
+require '../model/connection.php';
+
+try {
+$filter = [];
+ $option = [];
+ $read = new MongoDB\Driver\Query($filter, $option);
+ //Exécution de la requête
+ $cursor = $manager->executeQuery('planning.users', $read);
+ $line = $manager->executeQuery('planning.dates', $read);
+
+}catch (MongoDB\Driver\Exception\Exception $e) {
+    echo "Probleme! : ".$e->getMessage();
+ exit();
+
+}
+
+echo '<ul>';
+foreach ($cursor as $user){
+ echo '<li>'.$user->nom.'</li>';
+}
+echo '</ul>';
+
+echo '<table>';
+foreach ($line as $date) {
+    echo '<tr>';
+    $timeStamp = strtotime($date->date);
+    $formatted_date = date('d/m/y', $timeStamp);
+
+    echo '<td>'.$formatted_date.'</td>';
+
+   
+    echo '</tr>';
+
+};
+
+echo '</table>'
+
+
 
 ?>
