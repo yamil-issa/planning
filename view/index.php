1a69af26cf7c31f3d74dd5581689f04ca7d991d9
neo-askin <issayamilrt@gmail.com>
2023-02-19 15:53:30 +0100

    display user dropdown list

--- view/index.php
+++ view/index.php
@@ -25,7 +25,7 @@ try {
  $read = new MongoDB\Driver\Query($filter, $option);
 
  $query = new MongoDB\Driver\Query([], ['limit' => 52]);
- //Exécution de la requête
+ //Exécution des requêtes
  $cursor = $manager->executeQuery('planning.users', $read);
  $line = $manager->executeQuery('planning.dates', $query);
 
@@ -35,33 +35,38 @@ try {
 
 }
 
+$users_tab = [];
+
+   foreach ($cursor as $user) { 
+            $users_tab[] = $user;
+   
+} 
+
 
 ?>
 
-<select class="form-select" name="type" id="ty">
+
+
+<!--<select class="form-select" name="type" id="ty">
 <option value="nobody">personne</option>
-<?php
-        foreach ($cursor as $user) { 
-            $user_name = $user->nom;
-            $id_user = $user->_id;
-        ?>
-    <option value='<?= $id_user ?>'><?= $user_name ?></option>
-    <?php } ?>
-</select>
-
+<option value='<?= $user_id ?>'><?= $user_name ?></option>
+</select> !-->
 
 <?php
+
+$options = array_map(function ($users_tab) {
+    return "<option value=\"$users_tab->_id\">$users_tab->nom</option>";
+}, $users_tab);
+$select = "<select>" . implode("", $options) . "</select>";
 
 
 foreach ($line as $document) {
     $timestamp = strtotime($document->start_date);
     $date = date('d/m/Y', $timestamp);
+    ?>
 
-    echo '<ul>
-    <li>'.$date.' </li>
-    </ul>';
-}
+    <ul>
+     <li><?=$date ?>&nbsp;<?=$select ?></li>
+    </ul>
+<?php } ?>
 
-//echo '<p>Statistiques</p>'
-
-?>
